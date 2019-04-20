03e09f299721f89ea707c0c48f062fc77d69d488
neshimbekova <neshimbekova@gmail.com>
2019-04-20 17:04:14 -0500

    dsghgfdj

--- sg.tf
+++ sg.tf
@@ -4,13 +4,10 @@ resource "aws_security_group" "allow_ssh" {
   vpc_id      = "${aws_vpc.dev.id}"
 
   ingress {
-    # TLS (change to whatever ports you need)
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
   }
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["0.0.0.0/0"]
 
     egress {
