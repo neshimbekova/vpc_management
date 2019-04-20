913091f7d663bc49b2b6df2efa03a8dfcada29fe
neshimbekova <neshimbekova@gmail.com>
2019-04-20 16:20:22 -0500

    added 2nd subnet

--- subnet.tf
+++ subnet.tf
@@ -2,4 +2,22 @@ resource "aws_subnet" "dev1" {
   vpc_id = "${aws_vpc.dev.id}"
   cidr_block = "10.0.1.0/24"
 
+tags {
+    name       = "terraform-${var.created_by }-${count.index +1}"
+    env        = "${var.env}"
+    dept       = "${var.dept}"
+    created_by = "${var.created_by}"
+  }
+resource "aws_subnet" "dev1" {
+  vpc_id = "${aws_vpc.dev.id}"
+  cidr_block = "10.0.2.0/24"
+tags {
+    name       = "terraform-${var.created_by }-${count.index +1}"
+    env        = "${var.env}"
+    dept       = "${var.dept}"
+    created_by = "${var.created_by}"
+}
+
+}
+
 }
