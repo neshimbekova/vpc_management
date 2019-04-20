818d0dbbcbea0c9053cfc5aaa2d111a4f7dcf99b
neshimbekova <neshimbekova@gmail.com>
2019-04-20 16:21:33 -0500

    chnges

--- subnet.tf
+++ subnet.tf
@@ -8,6 +8,7 @@ tags {
     dept       = "${var.dept}"
     created_by = "${var.created_by}"
   }
+}
 resource "aws_subnet" "dev1" {
   vpc_id = "${aws_vpc.dev.id}"
   cidr_block = "10.0.2.0/24"
@@ -20,4 +21,3 @@ tags {
 
 }
 
-}
