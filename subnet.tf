6d6fe2386c4911983ae16ec2fdfe8e5929a9dde5
neshimbekova <neshimbekova@gmail.com>
2019-04-20 16:28:03 -0500

    test

--- subnet.tf
+++ subnet.tf
@@ -9,7 +9,7 @@ tags {
     created_by = "${var.created_by}"
   }
 }
-resource "aws_subnet" "dev1" {
+resource "aws_subnet" "dev2" {
   vpc_id = "${aws_vpc.dev.id}"
   cidr_block = "10.0.2.0/24"
 tags {
