ef620fd8edd278a157945aca4d70c9db706f7e46
neshimbekova <neshimbekova@gmail.com>
2019-04-20 16:57:37 -0500

    sdgf

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web" {
   key_name      = "${var.key_name}"
   subnet_id = "${aws_subnet.dev1.id}"
   associate_public_ip_address = "true"
-  vpc_security_group_ids = ["${aws_security_group.allow_tls}"]
+  vpc_security_group_ids = ["${aws_security_group.allow_tcp}"]
 
   tags {
     name       = "terraform-${var.created_by }-${count.index +1}"
