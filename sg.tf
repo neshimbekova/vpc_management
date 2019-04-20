be97aa8fc8dcd60a13022d81e04fd0d2e4ea1bc3
neshimbekova <neshimbekova@gmail.com>
2019-04-20 17:08:44 -0500

    gfdhf

--- sg.tf
+++ sg.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "allow_ssh" {
     egress {
     from_port       = 0
     to_port         = 0
-    protocol        = "-1"
+    protocol        = "tcp"
     cidr_block     = ["0.0.0.0/0"]
     }
 }
