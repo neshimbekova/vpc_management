06085d6213a5c8526174f25db9c7b821ab02b373
neshimbekova <neshimbekova@gmail.com>
2019-04-21 10:51:27 -0500

    manor changes

--- asg.tf
+++ asg.tf
@@ -11,13 +11,14 @@ module "asg" {
   security_groups = ["${aws_security_group.allow_ssh.id}"]
 
   # Auto scaling group
-  asg_name                  = "example-asg"
-  vpc_zone_identifier       = ["${aws_subnet.dev1.id}"]
-  health_check_type         = "EC2"
-  min_size                  = "${var.min_size}"
-  max_size                  = "${var.max_size}"
-  desired_capacity          = "${var.desired_capacity}"
-  wait_for_capacity_timeout = 0
+  asg_name                    = "example-asg"
+  vpc_zone_identifier         = ["${aws_subnet.dev1.id}"]
+  health_check_type           = "EC2"
+  min_size                    = "${var.min_size}"
+  max_size                    = "${var.max_size}"
+  desired_capacity            = "${var.desired_capacity}"
+  wait_for_capacity_timeout   = 0
+  associate_public_ip_address = "true"
 
   #   tags {
   #     name       = "terraform-${var.created_by }-${count.index +1}"
